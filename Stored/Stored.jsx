2be4821f7580200ac87d75285d5cdae59fb137ab
couch355 <tovareduardo75@gmail.com>
2024-01-06 22:11:50 -0600

    Update

--- Stored/Stored.jsx
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useStoreText = create((set) => 
-({
-    transLate: '',
-    translateText: (value) => set(state => ({
-        transLate: state.transLate = value
-    }))
-}))
